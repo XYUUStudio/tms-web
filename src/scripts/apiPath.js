dab6544596f014fc85b43777f1f576eb539a3015
shenjieming <495100766@qq.com>
2016-07-08 09:16:19 +0800

    400录单 调度取消 代码 提交

--- src/scripts/apiPath.js
+++ src/scripts/apiPath.js
@@ -30,12 +30,19 @@ var ApiPath = {
         },
         dispatchingManagement: {
             //调度管理接口
-            searchDispatchingList: AppConfig.TMSBasePath + "/consignment/search",
+            searchDispatchingList:AppConfig.TMSBasePath+"/consignment/search",
             //派单
-            dispatch: AppConfig.TMSBasePath + "/consignment/dispatch"
+            dispatch:AppConfig.TMSBasePath+"/consignment/dispatch",
+            //改派
+            modifyDispatch:AppConfig.TMSBasePath+"/consignment/modifyDispatch",
+            //运单提交
+            consignmentCommit:AppConfig.TMSBasePath+"/consignment/commit",
+            //运单取消
+            consignmentCancel:AppConfig.TMSBasePath+"/consignment/cancel",
         },
         dictionary: {
             //公共字典
+            GetDictionary:AppConfig.TMSBasePath+"/systemData/dictionary/queryType",
             //省市区
             admDivisionInfoSearch: AppConfig.TMSBasePath + "/systemData/dictionary/admDivisionInfoSearch",
             //图片上传
@@ -43,63 +50,10 @@ var ApiPath = {
             //司机
             getPickupDriverList: AppConfig.TMSBasePath + "/user/search",
         },
+        systemData:{
+            //运单状态
+            queryType:AppConfig.TMSBasePath+"/systemData/status/cgStatusTypeList",
+        },
         orderSearch: AppConfig.TMSBasePath + "/dispatchingManagement/search",
     }
-		TMSApi:{
-            UP:{
-                //登录
-                pcLogin:AppConfig.TMSBasePath+"/account/pcLogin",
-            },
-            businessData:{
-                //企业列表
-                enterprisesList:AppConfig.TMSBasePath+"/businessData/enterprises/search",
-                //企业详情
-                enterprisesDetail:AppConfig.TMSBasePath+"/businessData/enterprises/detail",
-                //新增
-                enterprisesRegister:AppConfig.TMSBasePath+"/businessData/enterprises/add",
-                //编辑
-                enterprisesModify:AppConfig.TMSBasePath+"/businessData/enterprises/modify",
-                //启用/禁用
-                enterprisesEnableDisable:AppConfig.TMSBasePath+"/businessData/enterprises/enableDisable"
-            },
-            user: {
-                //用户列表
-                userList: AppConfig.TMSBasePath + "/user/search",
-                //用户详情
-                userDetail: AppConfig.TMSBasePath + "/user/detail",
-                //重置密码
-                userReset: AppConfig.TMSBasePath + "/user/resetPsw",
-                //用户添加
-                userAdd: AppConfig.TMSBasePath + "/user/add",
-                //用户编辑
-                userEdit: AppConfig.TMSBasePath + "/user/modify"
-            },
-            dispatchingManagement:{
-                //调度管理接口
-                searchDispatchingList:AppConfig.TMSBasePath+"/consignment/search",
-                //派单
-                dispatch:AppConfig.TMSBasePath+"/consignment/dispatch",
-                //改派
-                modifyDispatch:AppConfig.TMSBasePath+"/consignment/modifyDispatch",
-                //运单提交
-                consignmentCommit:AppConfig.TMSBasePath+"/consignment/commit",
-                //运单取消
-                consignmentCancel:AppConfig.TMSBasePath+"/consignment/cancel",
-             },
-            dictionary:{
-               //公共字典
-                GetDictionary:AppConfig.TMSBasePath+"/systemData/dictionary/queryType",
-               //省市区
-                admDivisionInfoSearch:AppConfig.TMSBasePath+"/systemData/dictionary/admDivisionInfoSearch",
-                //图片上传
-                upload:AppConfig.TMSBasePath+"/upload/basedata",
-                //司机
-                getPickupDriverList:AppConfig.TMSBasePath+"/user/search",
-              },
-            systemData:{
-                //运单状态
-                queryType:AppConfig.TMSBasePath+"/systemData/status/cgStatusTypeList",
-            },
-            orderSearch: AppConfig.TMSBasePath + "/dispatchingManagement/search"
-		}
 }
